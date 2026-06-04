e57c75c8b07c6b16b6a97c1af0cd506a0289e569
goch11a <gochagabitashvili8@gmail.com>
2026-06-04 23:25:09 +0400

    still normal

--- application/urls.py
+++ application/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import ProductViewset, OrderViewset, OrderItemViewset
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from django.http import HttpResponse
 
 root = DefaultRouter()
 root.register(r'product', ProductViewset, basename='product')
@@ -9,6 +10,9 @@ root.register(r'order', OrderViewset, basename='order')
 root.register(r'orderitem', OrderItemViewset, basename='orderitem')
 
 
+
+def home(request):
+    return HttpResponse("Django is running 🚀")
 
 
 urlpatterns = [
