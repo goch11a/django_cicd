bfedfc6da74b01bbe536862252ef7c1a5e804802
goch11a <gochagabitashvili8@gmail.com>
2026-06-05 00:40:38 +0400

    delete application urls.py

--- CICDproject/urls.py
+++ CICDproject/urls.py
@@ -1,23 +1,21 @@
-"""
-URL configuration for CICDproject project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from application import views
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+root = DefaultRouter()
+root.register(r'product', views.ProductViewset, basename='product')
+root.register(r'order', views.OrderViewset, basename='order')
+root.register(r'orderitem', views.OrderItemViewset, basename='orderitem')
+
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('application.urls')),
+    path('', include(root.urls)),
+    path('admin/', admin.site.urls),   
+    path('login/', obtain_auth_token, name='api-auth-token'),
 ]
